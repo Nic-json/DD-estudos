d71dd7233ed133861e02d666d9737834486fd0ec
nic-json <nicolasmullerbohn@gmail.com>
2025-10-10 02:08:12 -0300

    2.0

---
+++ Modelo/Cadastros/Instituicao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Cadastros
+{
+    public class Instituicao
+    {
+        public long InstituicaoID { get; set; }
+        public string Nome { get; set; }
+        public String Endereco { get; set; }
+        public virtual ICollection<Departamento>? Departamento { get; set; }
+    }
+}
